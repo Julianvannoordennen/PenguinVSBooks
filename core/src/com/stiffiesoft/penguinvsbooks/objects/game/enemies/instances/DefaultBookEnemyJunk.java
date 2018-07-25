95120af8e77fb2ce7a6657bed842b24da10d8adb
Julian van Noordennen <jnoorden@avans.nl>
2018-07-25 11:57:50 +0200

    feature-junk v0.1.1
    - Randomized Enemy Dying junk

--- core/src/com/stiffiesoft/penguinvsbooks/objects/game/enemies/instances/DefaultBookEnemyJunk.java
+++ core/src/com/stiffiesoft/penguinvsbooks/objects/game/enemies/instances/DefaultBookEnemyJunk.java
@@ -1,5 +1,6 @@
 package com.stiffiesoft.penguinvsbooks.objects.game.enemies.instances;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.stiffiesoft.penguinvsbooks.objects.game.junk.FlyingJunk;
 import com.stiffiesoft.penguinvsbooks.objects.game.junk.JunkList;
 import com.stiffiesoft.penguinvsbooks.objects.game.junk.JunkPackage;
@@ -20,8 +21,12 @@ public class DefaultBookEnemyJunk extends JunkPackage {
     protected void loadJunk() {
 
         //Load all junk parts that will appear
-        junk.add(new FlyingJunk(transform.clone(),junkList,A.m.get(defaultBookEnemyJunk1)));
-        junk.add(new FlyingJunk(transform.clone(),junkList,A.m.get(defaultBookEnemyJunk2)));
-        junk.add(new FlyingJunk(transform.clone(),junkList,A.m.get(defaultBookEnemyJunk2)));
+        junk.add(new LyingJunk(transform.clone(),junkList,A.m.get(defaultBookEnemyJunk1)));
+
+        //Randomize some different junk parts
+        if (MathUtils.randomBoolean()) junk.add(new FlyingJunk(transform.clone(),junkList,A.m.get(defaultBookEnemyJunk1)));
+        if (MathUtils.randomBoolean()) junk.add(new FlyingJunk(transform.clone(),junkList,A.m.get(defaultBookEnemyJunk2)));
+        if (MathUtils.randomBoolean()) junk.add(new FlyingJunk(transform.clone(),junkList,A.m.get(defaultBookEnemyJunk2)));
+        if (MathUtils.randomBoolean()) junk.add(new FlyingJunk(transform.clone(),junkList,A.m.get(defaultBookEnemyJunk2)));
     }
 }
